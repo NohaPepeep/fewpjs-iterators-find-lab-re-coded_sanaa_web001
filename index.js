a6ec0657b50b46d743634b56a1e6168adbf9a60a
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-14 09:25:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(arr){
+  const newarr=arr.find(a.result==="W");
+  if(newarr==="W"){
+    return newarr.year;
+  }
+  else{
+    return newarr;
+  }
+}
